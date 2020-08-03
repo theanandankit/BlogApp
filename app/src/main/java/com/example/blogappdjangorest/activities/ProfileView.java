29b2fb23915f5db749c29f9ffbedb252197a7b86
theanandankit <ankitanand2909@gmail.com>
2020-08-03 19:16:01 +0530

    profile view added

---
+++ app/src/main/java/com/example/blogappdjangorest/activities/ProfileView.java
@@ -0,0 +1,16 @@
+package com.example.blogappdjangorest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.blogappdjangorest.R;
+
+public class ProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_view);
+    }
+}
