84ec9bb9ce808956cdbe068f396fb0e24c70983f
theanandankit <ankitanand2909@gmail.com>
2020-08-04 16:15:53 +0530

    UI update

--- app/src/main/java/com/example/blogappdjangorest/activities/ProfileView.java
+++ app/src/main/java/com/example/blogappdjangorest/activities/ProfileView.java
@@ -1,16 +1,27 @@
 package com.example.blogappdjangorest.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.blogappdjangorest.Adapter.HomeScreenAdapter;
 import com.example.blogappdjangorest.R;
 
 public class ProfileView extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        recyclerView=findViewById(R.id.recycleView);
+        HomeScreenAdapter homeScreenAdapter=new HomeScreenAdapter(getApplicationContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(homeScreenAdapter);
     }
 }
