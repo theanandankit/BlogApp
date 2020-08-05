ec7661beb61b5b1da92444b7382726d753d4107e
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-08-05 14:22:46 +0530

    Followers n Following

---
+++ app/src/main/java/com/example/blogappdjangorest/activities/FollowersNFollowing.java
@@ -0,0 +1,86 @@
+package com.example.blogappdjangorest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.blogappdjangorest.Adapter.Pager;
+import com.example.blogappdjangorest.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class FollowersNFollowing extends AppCompatActivity {
+
+
+    TabLayout tabLayout;
+    //This is our viewPager
+    ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_followers_n_following);
+
+        // When swiping between pages, select the
+        // corresponding tab.
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("Followers"));
+        tabLayout.addTab(tabLayout.newTab().setText("Following"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        //Initializing viewPager
+        viewPager = (ViewPager)findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                //actionBar.setSelectedNavigationItem(postion);
+                tabLayout.setScrollPosition(position,0,true);
+                tabLayout.setSelected(true);
+
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/blogappdjangorest/followernfollowing/Following.java
@@ -0,0 +1,38 @@
+package com.example.blogappdjangorest.followernfollowing;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.blogappdjangorest.Adapter.HomeScreenAdapter;
+import com.example.blogappdjangorest.R;
+public class Following extends Fragment {
+
+    RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_following, container, false);
+
+        recyclerView=view.findViewById(R.id.followingrecycle);
+        HomeScreenAdapter homeScreenAdapter=new HomeScreenAdapter(getActivity());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(homeScreenAdapter);
+        return view;
+
+    }
+}
