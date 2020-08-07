cacb69139f2961f2e8ae9ca5d175053fb4935e57
theanandankit <ankitanand2909@gmail.com>
2020-08-07 15:12:24 +0530

    retrofit

--- app/src/main/java/com/example/blogappdjangorest/activities/LoginScreen.java
+++ app/src/main/java/com/example/blogappdjangorest/activities/LoginScreen.java
@@ -7,17 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.blogappdjangorest.Dialog.CreateGroup;
+import com.example.blogappdjangorest.Models.RetrofitModels.LoginResponse;
 import com.example.blogappdjangorest.R;
+import com.example.blogappdjangorest.Retrofit.ApiClient;
+import com.example.blogappdjangorest.resources.PreferencesHelper;
 import com.example.blogappdjangorest.resources.WaitingDialog;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginScreen extends AppCompatActivity {
 
     MaterialButton login;
     TextInputLayout email,password;
     TextView forgot,signup;
+    ApiClient apiClient;
+    WaitingDialog waitingDialog;
+    PreferencesHelper preferencesHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +41,17 @@ public class LoginScreen extends AppCompatActivity {
         password=findViewById(R.id.login_password);
         forgot=findViewById(R.id.login_forget_password);
         signup=findViewById(R.id.login_signup_text);
+        apiClient = new ApiClient();
+        waitingDialog= new WaitingDialog(LoginScreen.this);
+        preferencesHelper=new PreferencesHelper(getApplicationContext());
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),HomeScreen.class));
+                if (check()) {
+                    waitingDialog.SetDialog("Authenticating...");
+                    waitingDialog.show();
+                    upload();
+                }
             }
         });
         forgot.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +60,59 @@ public class LoginScreen extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),ForgotPasswordRequest.class));
             }
         });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SignUpScreen.class));
+            }
+        });
 
+    }
+
+    public void upload()
+    {
+        Call<LoginResponse> call=apiClient.getApiinterface().login(email.getEditText().getText().toString(),password.getEditText().getText().toString());
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+
+                if (response.code()==200)
+                {
+                    if (!response.body().getToken().isEmpty()) {
+                        preferencesHelper.setToken(response.body().getToken());
+                        preferencesHelper.setid(response.body().getId());
+                        waitingDialog.dismiss();
+                        startActivity(new Intent(getApplicationContext(), HomeScreen.class));
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+            }
+        });
+    }
+    private boolean check()
+    {
+        if (!email.getEditText().getText().toString().isEmpty())
+        {
+            if (!password.getEditText().getText().toString().isEmpty())
+            {
+                return true;
+            }
+            else {
+                password.setError("This field can't be empty");
+                password.getEditText().setText("");
+                password.requestFocus();
+                return false;
+            }
+        }
+        else
+        {
+            email.setError("This Field can't be empty");
+            email.requestFocus();
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/blogappdjangorest/activities/SignUpScreen.java
+++ app/src/main/java/com/example/blogappdjangorest/activities/SignUpScreen.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.blogappdjangorest.Models.RetrofitModels.SignUpResponse;
 import com.example.blogappdjangorest.R;
+import com.example.blogappdjangorest.Retrofit.ApiClient;
+import com.example.blogappdjangorest.resources.PreferencesHelper;
+import com.example.blogappdjangorest.resources.WaitingDialog;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpScreen extends AppCompatActivity {
     TextInputLayout email,password,repassword,username,firstname,lastname;
     MaterialButton button;
     TextView text;
+    WaitingDialog waitingDialog;
+    PreferencesHelper preferencesHelper;
+    ApiClient apiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +42,20 @@ public class SignUpScreen extends AppCompatActivity {
         lastname=findViewById(R.id.signup_Last_layout);
         text=findViewById(R.id.signup_main_text);
         button=findViewById(R.id.signup_register);
+        preferencesHelper=new PreferencesHelper(getApplicationContext());
+        waitingDialog=new WaitingDialog(SignUpScreen.this);
+        apiClient=new ApiClient();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (email.getVisibility()==View.VISIBLE)
+                if (email.getVisibility()==View.VISIBLE&&check_initial())
                     final_condition();
                 else
                 {
-                    initial_condition();
-                    finish();
+                    if (check_final())
+                    {
+                        register();
+                    }
                 }
             }
         });
@@ -60,5 +77,101 @@ public class SignUpScreen extends AppCompatActivity {
         username.setVisibility(View.VISIBLE);
         firstname.setVisibility(View.VISIBLE);
         lastname.setVisibility(View.VISIBLE);
+    }
+    private boolean check_initial()
+    {
+        if (!email.getEditText().getText().toString().isEmpty())
+        {
+            if (!password.getEditText().getText().toString().isEmpty())
+            {
+                if (!repassword.getEditText().getText().toString().isEmpty())
+                {
+                    if (password.getEditText().getText().toString().equals(repassword.getEditText().getText().toString()))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        repassword.setError("Password must be same");
+                        repassword.getEditText().setText("");
+                        repassword.requestFocus();
+                        return false;
+                    }
+                }
+                else
+                {
+                    repassword.setError("password is required");
+                    repassword.requestFocus();
+                    return false;
+                }
+            }
+            else
+            {
+                password.setError("password is required");
+                password.requestFocus();
+                return false;
+            }
+        }
+        else
+        {
+            email.setError("Enter your Email");
+            email.requestFocus();
+            return false;
+        }
+    }
+    private boolean check_final()
+    {
+        if (!username.getEditText().getText().toString().isEmpty())
+        {
+            if (!firstname.getEditText().getText().toString().isEmpty())
+            {
+                if (!lastname.getEditText().getText().toString().isEmpty())
+                {
+                    return true;
+                }
+                else
+                {
+                    lastname.setError("This field Is required");
+                    lastname.requestFocus();
+                    return false;
+                }
+            }
+            else
+            {
+                firstname.setError("This field Is required");
+                firstname.requestFocus();
+                return false;
+            }
+        }
+        else
+        {
+            username.setError("Username is required");
+            username.requestFocus();
+            return false;
+        }
+    }
+    private void register()
+    {
+        Call<SignUpResponse> call=apiClient.getApiinterface().register(email.getEditText().getText().toString(),password.getEditText().getText().toString(),username.getEditText().getText().toString(),firstname.getEditText().getText().toString(),lastname.getEditText().getText().toString());
+        call.enqueue(new Callback<SignUpResponse>() {
+            @Override
+            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+
+                if (response.code()==200)
+                {
+                    if (!response.body().getToken().isEmpty())
+                    {
+                        waitingDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),"Successfully registered",Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
